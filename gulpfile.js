cd2171c120b4832137525ed07dae6e98914f13ec
Nick Largen <github@nicklargen.com>
2015-07-01 22:47:40 -0400

    Use browserSync to automatically reload browsers

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,37 @@
+'use-strict';
+
+var projectName = 'IScroll Improved';
+
 var gulp = require('gulp');
 var exec = require('child_process').exec;
+var browserSync = require("browser-sync").create(projectName);
+var reload = browserSync.reload;
 
-gulp.task('build', function() {
-    exec("node build.js")
+gulp.task('build', function () {
+    exec("node build.js");
 });
 
-gulp.task('watch', function () {
-    gulp.watch('./src/**/*.js', ['build']);
+gulp.task('serve', function () {
+    // .init starts the server
+    browserSync.init({
+        server: {
+            baseDir: ".",
+            index: "demos/scrollbars/index.html"
+        },
+        files: ["demos/**", "build/**"],
+        browser: ["chrome", "firefox"],
+        logLevel: 'info',
+        host: "192.168.1.12",
+        // Assume there is an active internet connection 
+        online: true,
+        // Don't show any notifications in the browser.
+        notify: false,
+        // Log connections
+        logConnections: true,
+        logPrefix: projectName
+    });
+    
+    gulp.watch('src/**', ['build']);
 });
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['serve']);
